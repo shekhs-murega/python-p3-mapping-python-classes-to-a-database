16491d4b374779f235e6a486b8bc8d233cca3d80
shekinah <nyamburamary1212@gmail.com>
2023-12-05 12:51:29 +0300

    Complete mapping python classes to a db

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from config import CONN, CURSOR
+from __init__ import CONN, CURSOR
 from song import Song
 
+import pytest; pytest.set_trace()
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
